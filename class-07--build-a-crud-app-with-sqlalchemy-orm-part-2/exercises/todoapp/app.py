36e6b188800530bbe5a9a142eb35f4c8987b980e
Filipe Bezerra <filipebzerra@gmail.com>
2019-12-04 04:25:45 -0300

    Shows lists

--- class-07--build-a-crud-app-with-sqlalchemy-orm-part-2/exercises/todoapp/app.py
+++ class-07--build-a-crud-app-with-sqlalchemy-orm-part-2/exercises/todoapp/app.py
@@ -36,7 +36,9 @@ def index():
 
 @app.route('/todos/<list_id>')
 def get_list_todos(list_id):
-    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+    return render_template('index.html',
+    lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id))
 
 
 @app.route('/todos/create', methods=['POST'])
