ca45f3f667e2d7f954e81df34c21b07b48552afb
Filipe Bezerra <filipebzerra@gmail.com>
2019-12-27 13:08:29 -0300

    Flaskr, a basic API with only a few endpoints

---
+++ module-03/exercises/flaskr/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return jsonify({'say': 'hELLo World'})
+
+    @app.route('/smiley')
+    def smiley():
+        return ':)'
+
+    return app
